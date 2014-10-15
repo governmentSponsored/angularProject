0613df38a42d4636817f024a3ffbf3dc927e3638
Stan Smith <stancsmith@gmail.com>
2014-10-15 14:54:49 -0400

    added beer object and app.controller reference...my first controller *single tear*

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-var app = angular.module('store',[]);
+//wrap the whole function in closure
+(function() {
+	var app = angular.module('store',[]);
+	
+	//important that StoreController be in caps and that it has Controller included in it
+	app.controller('StoreController', function() {
+		this.product = beer;
+	});
+	
+	var beer = {
+		name: "Rising Sun",
+		price: 4.99,
+		description: "Sour, citrus, and inspired by Japan, the land of the rising sun!"
+	}
+}();
