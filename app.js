095d00661704f8eb1bf0681c7e087a17d18532d5
Stan Smith <stancsmith@gmail.com>
2014-10-16 08:45:36 -0400

    added images to beer objects

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 	app.controller('StoreController', function() {
 		this.products = beers;
 	});
-	
+	// /angular/img/
 	var beers = [
 		{
 			name: "Rising Sun",
@@ -14,15 +14,31 @@
 			description: "Sour, citrus, and inspired by Japan, the land of the rising sun!",
 			canPurchase: true,
 			cannotPurchase: false,
-			soldOut: false
+			soldOut: false,
+			images: [
+				{
+					full: "risingSun.jpg",
+					thumb: "risingSun_thumb.jpg"
+				},
+				{
+					percent50: "risingSun_50.jpg",
+					percent25: "risingSun_25.jpg"
+				}
+			]
 		},
 		{
 			name: "Silver Bullet",
-			price: 0.50,
+			price: 0.5,
 			description: "Mmmm, cold. Cause that's the important thing here.",
 			canPurchase: true,
 			cannotPurchase: false,
-			soldOut: false
+			soldOut: false,
+			images: [
+				{
+					full: "silverBullet.jpg",
+					thumb: "silverBullet_thumb.jpg"
+				}
+			]
 		},
 		{
 			name: "Two Hearted",
@@ -30,7 +46,17 @@
 			description: "So darn good. Hoppy and delicious.",
 			canPurchase: true,
 			cannotPurchase: false,
-			soldOut: true
+			soldOut: true,
+			images: [
+				{
+					full: "twoHearted.jpg",
+					thumb: "twoHearted_thumb.jpg"
+				},
+				{
+					percent50: "twoHearted_50.jpg",
+					percent25: "twoHearted_25.jpg"
+				}
+			]
 		}			
 	];
 })();
