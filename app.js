18a39d2b9eeb20d0ec4ce01ce6f9fd3cc5821765
Stan Smith <stancsmith@gmail.com>
2014-10-17 08:52:12 -0400

    can now enter reviews and submit them into the page, but data does not persist...yet

--- app.js
+++ app.js
@@ -127,5 +127,11 @@
 	
 	app.controller("ReviewController", function() {
 		this.review = {};
+		
+		this.addReview = function(product) {
+			product.reviews.push(this.review);
+			//clears values in the form
+			this.review = {};
+		};
 	});
 })();
