6df1fcc854733e921b2a7e089426e644e3e352c8
Stan Smith <stancsmith@gmail.com>
2014-11-03 14:24:09 -0500

    convert jquery workaround to angularjs way of updating reviews

--- js/products.js
+++ js/products.js
@@ -56,8 +56,77 @@
 	app.directive('productReviews', function() {
 		return {
 			restrict: 'E',
-			templateUrl: './html/product-reviews.html'
+			templateUrl: './html/product-reviews.html',
+			controller: function($scope, BrewReviewService) {
+				$scope.parseData = [];
+				this.review = {};
+				var Review = Parse.Object.extend("Review");
+				
+				$scope.callParseData = function(beerType) {
+				BrewReviewService.brewReviewFromParse(beerType)
+								 .then(function(data) {
+									$scope.parseData = data.results;
+								 }, function(data) {
+									alert(data);
+								 })	
+				}
+				
+				//appends review w/date to the corresponding beer
+				$scope.addReview = function(product) {
+					var author = this.review.author,
+					stars = this.review.stars,
+					comment = this.review.comment,
+					beer = product.name
+
+					var reviewObject = new Review();
+					  reviewObject.save({
+						author: author,
+						stars: stars,
+						comment: comment,
+						beer: beer
+					  }, {
+					  success: function(object) {
+						//$scope.parseData.push(reviewObject);
+						$scope.callParseData(beer);
+						console.log('saved!');
+					  },
+					  error: function(model, error) {
+						console.log('not saved!');
+					  }
+					}); 
+					
+					//clears values in the form and cleans it up
+					this.review = {};
+					$scope.reviewForm.$setPristine();
+				}
+			},
+			controllerAs: 'BrewReviewCtrl'
 		};
+	});
+	
+	//service method instead of factory. seems to be more straightforward.
+	app.service('BrewReviewService', function($http, $q){
+		 
+		this.brewReviewFromParse = function(beerType) {
+			var deferred = $q.defer();
+			var encoded = encodeURIComponent('where={"beer":"'+ beerType +'"}');
+			var baseUrl = 'https://api.parse.com/1/classes/Review?';
+			var fullUrl = baseUrl + encoded;
+			
+			$http({method : 'GET',
+				   url : fullUrl, 
+				   headers: { 'X-Parse-Application-Id':'WfjtyO2ov01ie5KPiSbOaAvOzBpessMB8iervPEi', 'X-Parse-REST-API-Key':'Gc8NJ6LtoyZ7JBXbT6GYKUABWcXFIltFti7qxhqm'}
+				 })
+				.success(function(data, status) {
+					deferred.resolve(data);
+				})
+				.error(function(data, status) {
+					deferred.reject('There was an error');
+				})
+				
+			return deferred.promise;
+		}
+	 
 	});
 	
 	function prettyDateFormat(d) {
@@ -70,8 +139,44 @@
 			return curr_month + ' ' + curr_date + ", " + curr_year;
 	}
 
+	//get the list of beers for the interface
+	app.service('BeerService', function($http, $q){
+		 
+		this.beersFromParse = function() {
+			var deferred = $q.defer();
+			var baseUrl = 'https://api.parse.com/1/classes/Beer?';
+			
+			$http({method : 'GET',
+				   url : baseUrl, 
+				   headers: { 'X-Parse-Application-Id':'WfjtyO2ov01ie5KPiSbOaAvOzBpessMB8iervPEi', 'X-Parse-REST-API-Key':'Gc8NJ6LtoyZ7JBXbT6GYKUABWcXFIltFti7qxhqm'}
+				 })
+				.success(function(data, status) {
+					deferred.resolve(data);
+				})
+				.error(function(data, status) {
+					deferred.reject('There was an error');
+				})
+				
+			return deferred.promise;
+		}
+	 
+	});
+
+	app.controller('BeerController', function($scope, BeerService) {
+		$scope.beerData = [];
+		
+		$scope.callParseBeerData = function() {
+			BeerService.beersFromParse()
+							 .then(function(data) {
+								$scope.beerData = data.results;
+							 }, function(data) {
+								alert(data);
+							 })
+		}		
+	});
+	
 	//adds new reviews and clears the form once review is added
-	app.controller("ReviewController", function($scope) {
+/*	app.controller("ReviewController", function($scope) {
 		this.review = {};
 		var Review = Parse.Object.extend("Review");
 		
@@ -83,6 +188,8 @@
 			beer = product.name,
 
 			//jQuery hack to update the beer reviews. Need to find Angular way of doing this.
+			//basically clone existing element, inject new values and then append to parent
+			//apending happens in success function below
 			el = $('[id="' + beer + '"]'), 
 			lastEl = el.last(),
 		    clone = lastEl.clone();
@@ -100,7 +207,8 @@
 		      }, {
 		      success: function(object) {
 		      	clone.find('.reviewDate').text(prettyDateFormat(reviewObject.createdAt));
-		      	lastEl.parent().append(clone);
+		      	//lastEl.parent().append(clone);
+				$scope.parseData.push(reviewObject);
 		      	console.log('saved!');
 		      },
 		      error: function(model, error) {
@@ -112,5 +220,5 @@
 			this.review = {};
 			$scope.reviewForm.$setPristine();
 		};
-	});
+	});*/
 })();
